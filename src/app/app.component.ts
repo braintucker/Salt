ebf26fba0a57c9f76ec4c2a07fe33f9773dfbaed
Brain Tucker <brian.briantucker@gmail.com>
2017-11-06 10:56:02 -0600

    added bootstrap styling for panels and lists

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 'angularfire2';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import  'rxjs/add/operator/take'
 
 @Component({
   selector: 'app-root',
@@ -11,14 +12,23 @@ import 'rxjs/add/operator/map';
 export class AppComponent {
   foods: FirebaseListObservable<any[]>;
   restaurants: Observable<any[]>;
+  exists;
 
   constructor(private af: AngularFire){
   }
 
   ngOnInit() {
-    this.foods = this.af.database.list('/foods');
+    this.foods = this.af.database.list('/foods', {
+      query: {
+        orderByKey: true
+      }
+    });
 
-    this.restaurants = this.af.database.list('/restaurants')
+    this.restaurants = this.af.database.list('/restaurants', {
+      query: {
+        orderByChild: 'name'
+      }
+    })
       .map(restaurants => {
         restaurants.map(restaurant => {
           restaurant.featureTypes = [];
@@ -27,5 +37,26 @@ export class AppComponent {
         });
         return restaurants;
       });
+
+    this.af.auth.subscribe(authState => {
+      if(!authState)
+        console.log("NOT LOGGED IN");
+      else
+        console.log("LOGGED IN", authState);
+    });
+  }
+
+  login() {
+    this.af.auth.login({
+      provider: AuthProviders.Facebook,
+      method: AuthMethods.Popup
+    }).then(authState => {
+      console.log("AFTER LOGIN:", authState)
+    }).catch((err) => {
+      console.log("ERROR:", err);
+    })
+  }
+  logout() {
+    this.af.auth.logout();
   }
 }
