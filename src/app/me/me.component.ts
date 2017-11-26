97a2a8ea8efdf93ed091c86291c9f0917ac5dd29
Brain Tucker <brian.briantucker@gmail.com>
2017-11-26 14:23:24 -0600

    content is now displayed whenever a user is logged in, it will not display unless you are logged in

--- src/app/me/me.component.ts
+++ src/app/me/me.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 'angularfire2';
+import { AngularFire, AuthProviders, AuthMethods} from 'angularfire2';
 import { Http } from '@angular/http';
 
 
@@ -11,9 +11,20 @@ import { Http } from '@angular/http';
 })
 export class Me implements OnInit {
 
+  loggedIn;
+
   constructor(private af: AngularFire, private http: Http){
   }
 
   ngOnInit() {
+    this.af.auth.subscribe(authState => {
+      if(!authState) {
+        this.loggedIn = false;
+        console.log("Logged in?", this.loggedIn);
+        return
+      }
+        this.loggedIn = true;
+        console.log("Logged in?", this.loggedIn);
+    });
   }
 }
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 'angularfire2';
+import { AngularFire, AuthProviders, AuthMethods} from 'angularfire2';
 import { Http } from '@angular/http';
+
 
 
 @Component({
@@ -11,9 +12,20 @@ import { Http } from '@angular/http';
 })
 export class Skills implements OnInit {
 
+  loggedIn;
+
   constructor(private af: AngularFire, private http: Http){
   }
 
   ngOnInit() {
+    this.af.auth.subscribe(authState => {
+      if(!authState) {
+        this.loggedIn = false;
+        console.log("Logged in?", this.loggedIn);
+        return
+      }
+        this.loggedIn = true;
+        console.log("Logged in?", this.loggedIn);
+    });
   }
 }
